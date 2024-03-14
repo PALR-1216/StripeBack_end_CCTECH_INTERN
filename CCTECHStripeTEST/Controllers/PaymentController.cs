131f791a5c55510df48dfcfdd480143743c4e861
Pedro Lorenzo <plorenzo2184@intersg.edu>
2024-03-14 13:27:58 -0400

    first init

---
+++ CCTECHStripeTEST/Controllers/PaymentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCTECHStripeTEST.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly StripeService _stripeService;
+
+        public PaymentController(StripeService stripeService)
+        {
+            _stripeService = stripeService;
+        }
+
+        [HttpPost("create-payment-intent")]
+        public ActionResult CreatePaymentIntent(int amount, string currency)
+        {
+            try
+            {
+                var clientSecret = _stripeService.GenerateClientSecret(amount, currency);
+                return Ok(new { clientSecret });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Failed to create payment intent.", error = ex.Message });
+            }
+        }
+    }
+}
---
+++ CCTECHStripeTEST/StripeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Stripe;
+namespace CCTECHStripeTEST
+{
+	public class StripeService
+	{
+		private readonly StripeClient _stripeClient;
+
+		public StripeService()
+		{
+			_stripeClient = new StripeClient("sk_test_51OrLO8Rq3xShrsxlJiSjvPjtHYapSWs18z3CaVhvos3ZPPdJ7Hdcsztc04UoBB1zPWsC2JpxFJ3Ny8A30NTiiCWE005z658TNa");
+
+		}
+
+		public string GenerateClientSecret(int amount, string currency)
+		{
+			var options = new PaymentIntentCreateOptions
+			{
+				Amount = amount,
+				Currency = currency,
+				PaymentMethodTypes = new List<string> { "card" }
+			};
+
+			var service = new PaymentIntentService(_stripeClient);
+			var paymentIntent = service.Create(options);
+
+
+			return paymentIntent.ClientSecret;
+		}
+	}
+}
+
